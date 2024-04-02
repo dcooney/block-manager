85aa4dc9b92b44d89cd6e7cde3371d1b6ad541b3
Darren Cooney <darren.cooney@gmail.com>
2024-04-02 11:45:59 -0400

    3.0 release prep

--- build/block-manager-admin.asset.php
+++ build/block-manager-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-library', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1a42663e0d5172a2cda3');
+<?php return array('dependencies' => array('wp-block-library', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e106b060f9e5e604f0c0');
