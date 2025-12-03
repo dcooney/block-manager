617421b04e9eeb42e5599320f829e2b0d7db95fc
Valentin Deville <contact@valentin-deville.fr>
2025-12-03 16:58:29 +0100

    build: Update build files

--- build/block-manager.asset.php
+++ build/block-manager.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-hooks'), 'version' => '221224ee4a0e4fe9c64e');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-hooks'), 'version' => '55e2556168d003e84d9b');
